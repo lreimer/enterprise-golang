4c6b54cb853de2b7e4b20ade2dd298fb629ed22c
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2020-01-20 17:36:56 +0100

    First running Gin service skeleton.

---
+++ ginweb-service/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	engine := gin.Default()
+
+	// configuration for static files and templates
+	engine.LoadHTMLGlob("templates/*.html")
+	engine.StaticFile("/favicon.ico", "favicon.ico")
+
+	engine.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "A Hitchhiker's Guide to Enterprise Microservices with Go",
+		})
+	})
+
+	// run server on PORT
+	engine.Run(port())
+}
+
+func port() string {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	return ":" + port
+}
